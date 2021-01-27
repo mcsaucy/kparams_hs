cdaddd55357c7856ac69de22dd75c064557467d8
Josh McSavaney <me@mcsau.cc>
2021-01-27 12:42:50 -0500

    add some cabal2nix bits

---
+++ default.nix
@@ -0,0 +1,14 @@
+{ mkDerivation, base, HUnit, stdenv }:
+mkDerivation {
+  pname = "kparams-hs";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  libraryHaskellDepends = [ base ];
+  executableHaskellDepends = [ base ];
+  testHaskellDepends = [ base HUnit ];
+  doHaddock = false;
+  description = "Parses values from /proc/cmdline";
+  license = stdenv.lib.licenses.mit;
+}
