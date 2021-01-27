6e1e149470d692bfae2e86208160fe49e5405eb7
Josh McSavaney <me@mcsau.cc>
2021-01-27 11:19:07 -0500

    Wire up main to read /proc/cmdline

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,28 @@
 module Main where
 
+import System.Environment
+import System.IO
+import Control.Monad
 import Kparams.Parse
 
+argParse :: [String] -> (String, Maybe String)
+argParse (name:[])     = (name, Nothing)
+argParse (name:def:[]) = (name, Just def)
+argParse _             = error "Usage: kparams param_name [default]"
+
+kp :: String -> String -> Maybe String -> String
+kp _       ""   _ = error "param_name must not be blank"
+kp cmdline name def =
+   case find name cmdline of
+     Just v -> v
+     Nothing -> case def of
+                  Just d -> d
+                  Nothing -> error $ name ++ " is not set"
+
 main :: IO ()
-main =
-  putStrLn $ "root: " ++ case (find "root" txt) of
-                           Nothing -> "nil"
-                           Just p -> p
-  where txt = "root=foo'bar baz' lol this is great"
-        split = wordsplit txt
+main = do
+  handle <- openFile "/proc/cmdline" ReadMode
+  cmdline <- hGetContents handle
+  args <- getArgs
+  let (name, def) = argParse args
+  putStrLn $ kp cmdline name def
